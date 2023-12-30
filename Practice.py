1d67a703fb3d1aa2d970fa5bbc1811b3d3d57d08
tayyabaeman <tayyabaeman24@gmail.com>
2023-12-30 15:24:36 +0500

    first commit

---
+++ Practice.py
@@ -0,0 +1,348 @@
+""" File 2 """
+
+""" Task 1 """
+"""X=13
+print('Variable value:',X)
+print('Press any key to continue...')"""
+
+""" Task 2 """
+"""X=int(input('Enter Variabe Value:'))
+print('Variable value Entered:',X)
+print('Press any key to continue...')"""
+
+""" Task 3 """
+"""A=int(input('Enter value of A:'))
+B=int(input('Enter value of B:'))
+Sum=A+B
+Difference=A-B
+Product=A*B
+Divide=A/B
+print('Sum is:',Sum)
+print('Difference is:',Difference)
+print('Product is:',Product)
+print('Division is:',Divide)"""
+
+""" Task 4 """
+"""X=int(input('Table of:'))
+print(X,"*",1,"=",X*1)
+print(X,"*",2,"=",X*2)
+print(X,"*",3,"=",X*3)
+print(X,"*",4,"=",X*4)
+print(X,"*",5,"=",X*5)
+print(X,"*",6,"=",X*6)
+print(X,"*",7,"=",X*7)
+print(X,"*",8,"=",X*8)
+print(X,"*",9,"=",X*9)
+print(X,"*",10,"=",X*10)"""
+
+""" Task 5 """
+"""Fuel=int(input('Enter value of Fuel:'))
+Rent=int(input('Enter value of Rent:'))
+Bills=int(input('Enter value of Bills:'))
+Sum=Fuel+Rent+Bills
+print('---------------------------------------------------')
+print('Fuel:             ',Fuel)
+print('Rent:             ',Rent)
+print('Bills:            ',Bills)
+print('Total:            ',Sum)
+print('---------------------------------------------------')
+print('Press any key to continue...')"""
+
+""" Task 6 """
+"""Chemistry=int(input('Enter value of Chemistry:'))
+Maths=int(input('Enter value of Maths:'))
+Physics=int(input('Enter value of Physics:'))
+Sum=Chemistry+Maths+Physics
+print('---------------------------------------------------------')
+print('Maths'   ,   'Chemistry'   ,   'Physics'   ,   'Total')
+print((" "),Maths  ,("   "),Chemistry         ,("   "),Physics          ,("   "),Sum)
+print('---------------------------------------------------------')
+print('Press any key to continue...')"""
+
+""" Task 7 """
+"""Wheat_price=int(input('Enter price of Wheat:'))
+Wheat_quantity=int(input('Enter quantity of Wheat:'))
+Rice_price=int(input('Enter price of Rice:'))
+Rice_quantity=int(input('Enter quantity of Rice:'))
+Sugar_price=int(input('Enter price of Sugar:'))
+Sugar_quantity=int(input('Enter quantity of Sugar:'))
+Wheat_Value=Wheat_price*Wheat_quantity
+Rice_Value=Rice_price*Rice_quantity
+Sugar_Value=Sugar_price*Sugar_quantity
+print('------------------------------------------------')
+print('Value of Wheat:  ',Wheat_Value)
+print('Value of Rice:   ',Rice_Value)
+print('Value of Sugar:  ',Sugar_Value)
+print('------------------------------------------------')
+print('Press any key to continue...')"""
+
+""" Task 8 """
+"""A=int(input('Enter value of A:'))
+B=int(input('Enter value of B:'))
+C=int(input('Enter value of C:'))
+print('Reverse of A,B,C is:',C,B,A)"""
+
+""" Task 9 """
+"""Hours=int(input('Input Hours:'))
+Minutes=int(input('Input Minutes:'))
+Time=Hours*60+Minutes
+print('Total Minutes are:',Time)"""
+
+""" Task 10 """
+"""Minutes=int(input('Input Minutes:'))
+Hours=Minutes//60
+m=Minutes%60
+print(int(Hours),'Hours',int(m),'Minutes')"""
+
+""" Task 11 """
+"""X=15
+Y=21
+print('Value of X is:',X)
+print('Value of Y is:',Y)
+print('Sum of X+Y is:',X+Y)
+print('Difference of X-Y is:',X-Y)
+print('Difference of Y-X is:',Y-X)
+print('Multiplication of X*Y is:',X*Y)
+print('Division of X/Y is:',X/Y)
+print('Division of Y/X is:',Y/X)"""
+
+""" Task 12 """
+"""X=15
+Y=21
+Sum=X+Y
+Difference1=X-Y
+Difference2=Y-X
+Multiply=X*Y
+Divide1=X/Y
+Divide2=Y/X
+print('Sum is:',Sum)
+print('Difference1 is:',Difference1)
+print('Difference2 is:',Difference2)
+print('Multiplication is:',Multiply)
+print('Division1 is:',Divide1)
+print('Division2 is:',Divide2)"""
+
+""" Update program 12"""
+"""X=int(input('Enter value of X:'))
+Y=int(input('Enter value of Y:'))
+print('Value of X is:',X)
+print('Value of Y is:',Y)
+print('Sum of X+Y is:',X+Y)
+print('Difference of X-Y is:',X-Y)
+print('Difference of Y-X is:',Y-X)
+print('Multiplication of X*Y is:',X*Y)
+print('Division of X/Y is:',X/Y)
+print('Division of Y/X is:',Y/X)"""
+
+""" Again update program 12 """
+"""X=int(input('Enter value of X:'))
+Y=int(input('Enter value of Y:'))
+Sum=X+Y
+Difference1=X-Y
+Difference2=Y-X
+Multiply=X*Y
+Divide1=X/Y
+Divide2=Y/X
+print('Sum is:',Sum)
+print('Difference1 is:',Difference1)
+print('Difference2 is:',Difference2)
+print('Multiplication is:',Multiply)
+print('Division1 is:',Divide1)
+print('Division2 is:',Divide2)"""
+
+""" Task 13 """
+"""Lenght=15
+Width=21
+Perimeter=2*(Lenght+Width)
+Area=Lenght*Width
+print('Lenght of Rectangle is:',Lenght)
+print('Width of Rectangle is:',Width)
+print('Perimeter of Rectangle is:',Perimeter)
+print('Area of Rectangle is:',Area)"""
+
+""" Task 14 """
+"""Lenght=int(input('Please enter Lenght of Rectangle:'))
+Width=int(input('Please enter Width of Rectangle:'))
+Perimeter=2*(Lenght+Width)
+Area=Lenght*Width
+print('Lenght of Rectangle is:',Lenght)
+print('Width of Rectangle is:',Width)
+print('Perimeter of Rectangle is:',Perimeter)
+print('Area of Rectangle is:',Area)"""
+
+""" File 3 """
+""" Task 2 """
+"""A=int(input('Player 1:'))
+B=int(input('Player 2:'))
+Rock=1
+Paper=2
+Scissors=3
+if A==1 and B==1:
+    print('Tie.')
+elif A==1 and B==2:
+    print('Player 2 wins.')
+elif A==1 and B==3:
+    print('Player 1 wins.')
+elif A==2 and B==1:
+    print('Player 1 wins.')
+elif A==2 and B==2:
+    print('Tie.')
+elif A==2 and B==3:
+    print('Player 2 wins.')
+elif A==3 and B==1:
+    print('Player 2 wins.')
+elif A==3 and B==2:
+    print('Player 1 wins.')
+elif A==3 and B==3:
+    print('Tie.')
+else:
+    print('Invalid condition.')"""
+
+""" Task 3 """
+"""X=int(input('Enter value of X:'))
+Y=int(input('Enter value of Y:'))
+if X%Y==0:
+    print('its a multiple.')
+else:
+    print('its not a multiple.')"""
+
+""" Task 4 """
+"""X=int(input('Enter value of X:'))
+Y=int(input('Enter value of Y:'))
+Operator=str(input('Operations:'))
+if Operator=="+":
+    print('Sum is:',X+Y)
+elif Operator=="-":
+    print('Difference is:',X-Y)
+elif Operator=="*":
+    print('Product is:',X*Y)
+elif Operator=="/":
+    print('Division is:',X/Y)
+elif Operator=="%":
+    print('Moduluos is:',X%Y)
+else:
+    print('Invalid operator entered.')"""
+
+""" Task 5 """
+"""A=int(input('Enter value of A:'))
+B=int(input('Enter value of B:'))
+C=int(input('Enter value of C:'))
+if A>B>C:
+    print('2nd max value is B')
+elif C>B>A:
+    print('2nd max value is B')
+elif B>A>C:
+    print('2nd max value is A')
+elif C>A>B:
+    print('2nd max value is A')
+elif A>C>B:
+    print('2nd max value is C')
+elif B>C>A:
+    print('2nd max value is C')  
+else:
+    print('There is no 2nd max value.')"""
+
+""" Task 6 """
+"""A=int(input('Enter value of A:'))
+B=int(input('Enter value of B:'))
+C=int(input('Enter value of C:'))
+D=int(input('Enter value of D:'))
+E=int(input('Enter value of E:'))
+F=int(input('Enter value of F:'))
+if A>B and A>C and A>D and A>E and A>F:
+    print('A got highest average')
+elif B>A and B>C and B>D and B>E and B>F:
+    print('B got highest average')
+elif C>A and C>B and C>D and C>E and C>F:
+    print('C got highest average')
+elif D>A and D>B and D>C and D>E and D>F:
+    print('D got highest average')
+elif E>A and E>B and E>C and E>D and E>F:
+    print('E got highrst average')
+elif F>A and F>B and F>C and F>D and F>E:
+    print('F got highest average')
+else :
+    print('Neither section got highest average.')"""
+
+""" Task 7 """
+"""Height=int(input('Persons Height:'))
+Weight=int(input('Persons Weight:'))
+BMI=Weight/(Height*Height)
+if BMI<18.5:
+    print('Person is underweight')
+elif BMI>=18.5 and BMI<=24.9:
+    print('Person is normal')
+elif BMI>=25 and BMI<=29.9:
+    print('Person is overweight')
+elif BMI>=30:
+    print('Person is obese')
+else :
+    print('Invalid condition')"""
+
+""" Task 1 """
+'''user = float(input("Enter the value of Conversion :"))
+Conversion = str(input("Enter the Conversion type in inch , centimeter , gallon , liter, mile , kilometer ,pound ,ounce,Fahrenheit,Celsius:"))
+inch1 = 2.54
+centimeter1 = 0.393701 
+gallon1 = 3.78541 
+liter1 = 0.264172 
+mile1 = 1.60934 
+kilometer1 = 0.621371 
+pound1 = 0.453592 
+ounce1 = 0.0625
+pound = 16 # ounce
+Fahrenheit_to_Celsius = ()
+Celsius_to_Fahrenheit = ()
+if Conversion == 'inch':
+  print('convert from inches to centimeters :',float(2.54*user))
+elif Conversion == 'centimeter' :
+  print("convert from centimeters to inches :",float(0.393701*user))
+elif Conversion == 'gallon':
+  print('convert from gallons to liters :',float(3.78541*user))
+elif Conversion == "liter":
+  print("convert from liters to gallon :",float(0.264172*user))
+elif Conversion == "mile":
+  print("convert from mile to kilometer :",float(1.60934*user))
+elif Conversion == "kilometer":
+  print("convert from kilometer to mile :",float(0.621371 * user))
+elif Conversion == "pound":
+  print("convert from pound to kilogram :",float(pound1*user))
+elif Conversion == 'ounce':
+  print("convert from pound to ounce :",float(ounce1 * user))
+elif Conversion == 'Fahrenheit to Celsius':
+  Fahrenheit = input("Enter the Value of temperature in Fahrenheit :")
+  Celsius = ((Fahrenheit-32)*5)/9 
+  print("convert from Fahrenheit to Celsius :",float(Celsius))
+elif Conversion == 'Celsius to Fahrenheit':
+  Celsius = input("Enter the Value of temperature in Celsius :")
+  Fahrenheit = (1.8 * Celsius) + 32
+  print("convert from Celsius to Fahrenheit :",float(Fahrenheit))
+else:
+  print("Please enter the right Conversion ")'''
+
+""" File 4 """
+
+""" Task 1 """
+"""prompt="Enter a Number:"
+n=int(input(prompt))
+X=[]
+for i in range(-n,n+1):
+    z=abs(i)
+    print(z)
+    X.append(z)
+    Y=str(X)
+    Y.replace('[',' ')
+    Y.replace(']',' ')
+    print(Y)"""
+
+""" Task 2 """
+X=int(input('Enter a number:'))
+Y=int(input('Enter a number:'))
+for i in range(X,Y+1):
+    if i%3==0 and i%5==0:
+        print('Number divisible by 3 or 5 are: ',i)
+for i in range(Y+1,X):
+    if i%5==0 and i%3==0:
+        print('Number divisible by 3 or 5 are: ',i)
+
+
